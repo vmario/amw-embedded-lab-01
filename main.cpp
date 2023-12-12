6300dd517d4a84421ed46665861bc6efda72f079
Mariusz Chilmon <vmario1986@gmail.com>
2023-12-12 22:01:40 +0100

    Upgrade the code

--- main.cpp
+++ main.cpp
@@ -1,27 +1,17 @@
 #include <avr/io.h>
 
-#include <stdlib.h>
-
 /**
- * Numery pinów powiązane z LED-ami.
+ * Numery pinów, do których podłączone są LED-y.
  */
-enum PIN_LED {
-	PIN_LED1 = 0,
-	PIN_LED2 = 1,
-	PIN_LED3 = 2,
-	PIN_LED4 = 3,
-	PIN_LED5 = 4,
+enum LED {
+	LED_D2 = 4, ///< Dioda D2.
 };
 
 /**
- * Numery pinów powiązane z przyciskami.
+ * Numery pinów, do których podłączone są przyciski.
  */
-enum PIN_KEY {
-	PIN_KEY1 = 3,
-	PIN_KEY2 = 4,
-	PIN_KEY3 = 5,
-	PIN_KEY4 = 6,
-	PIN_KEY5 = 7,
+enum SWITCH {
+	SWITCH_A1 = 1, ///< Przycisk A1.
 };
 
 /**
@@ -29,9 +19,9 @@ enum PIN_KEY {
  * 
  * @param pinLed Wskazany pin.
  */
-void ledOn(enum PIN_LED pinLed)
+void ledOn(uint8_t pinLed)
 {
-	PORTC &= ~_BV(pinLed);
+	PORTB &= ~_BV(pinLed);
 }
 
 /**
@@ -39,17 +29,18 @@ void ledOn(enum PIN_LED pinLed)
  * 
  * @param pinLed Wskazany pin.
  */
-void ledOff(enum PIN_LED pinLed)
+void ledOff(uint8_t pinLed)
 {
-	PORTC |= _BV(pinLed);
+	PORTB |= _BV(pinLed);
 }
 
 /**
- * Inicjalizuje porty GPIO.
+ * Inicjalizuje GPIO.
  */
 void gpioInitialize()
 {
-	DDRC = _BV(PIN_LED1);
+	DDRB = _BV(LED_D2);
+	ledOn(LED_D2);
 }
 
 /**
@@ -65,11 +56,8 @@ void mainLoop()
 int main()
 {
 	gpioInitialize();
-	ledOn(PIN_LED1);
 
 	while (true) {
 		mainLoop();
 	}
-
-	return EXIT_SUCCESS;
 }
